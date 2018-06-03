741ce9daef0ee4a9fb37ca069dc30345bba9e5b3
Jakub Lankiewicz <lankiewiczjakub@gmail.com>
2018-06-03 20:11:01 +0200

    web

---
+++ src/containers/TodoList.js
@@ -0,0 +1,7 @@
+﻿import React from "react";
+
+const TodoList = props => { 
+	const todoListElements = props.list.map(element => <li key={element.id} id={element.id} onClick={() => props.remove(element.id)}> {element.text} </li>);
+	return ( <ul>{todoListElements}</ul> ) }
+
+export default TodoList;
